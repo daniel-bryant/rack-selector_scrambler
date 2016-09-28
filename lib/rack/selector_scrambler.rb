ed04015b9bf72354e01aa5bc0f241b822d0bb96d
Daniel Bryant <bryant.daniel.j@gmail.com>
2016-09-27 23:59:00 -0700

    initial commit

---
+++ lib/rack/selector_scrambler.rb
@@ -0,0 +1,7 @@
+require "rack/selector_scrambler/version"
+
+module Rack
+  module SelectorScrambler
+    # Your code goes here...
+  end
+end
---
+++ spec/rack/selector_scrambler_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Rack::SelectorScrambler do
+  it "has a version number" do
+    expect(Rack::SelectorScrambler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
